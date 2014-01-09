c3e4a59c03cbc75a1720c4c833a71d1ff8b385f0
jaboc <jacob@purecontent.org>
2014-01-09 09:31:59 +0000

    Better

--- mpdnotify.py
+++ mpdnotify.py
@@ -12,11 +12,45 @@ def sendmessage(title, message):
 client = mpd.MPDClient(use_unicode=True)
 client.connect("localhost", 6600)
 
-current = ""
+currentTrack 	= ""
+currentState 	= "badger"
+previousState 	= "ferret"
 
 while True:
-	if client.idle('player') != "":
+	if client.idle('player') != "" :
 		d = client.currentsong()
-		if d['title'] != current:
-			sendmessage( d['artist'],d['title'] +"\n"+ d['album'] +"\n"+ str(datetime.timedelta(seconds=int(d['time']))) )
-			current = d['title']
+		noted = "false"
+		
+		
+	if client.status()['state'] == "pause":
+		previousState = currentState
+		currentState = client.status()['state']
+		print ("Previous : " + previousState)
+		print ("Current  : " + currentState)
+		
+	if client.status()['state'] == "play" :
+		try:
+			artist = d['artist']
+			title  = d['title']
+			album  = d['album']
+			time   = d['time']
+		except KeyError:
+			artist = ""
+			title  = ""
+			album  = ""
+			time   = ""
+		
+		if d['title'] != currentTrack and noted == "false":		
+			sendmessage( artist , title +"\n"+ album +"\n"+ str(datetime.timedelta(seconds=int(time))) )
+			currentTrack = title
+			previousState = currentState
+			currentState = client.status()['state']
+			noted = "true"
+		else:
+			if d['title'] == currentTrack and currentState != previousState:
+				sendmessage( artist , title +"\n"+ album +"\n"+ str(datetime.timedelta(seconds=int(time))) )
+				currentTrack = title
+				previousState = currentState
+				currentState = client.status()['state']
+				noted = "true"
+
